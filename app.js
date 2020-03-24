53064680c95909f39cb646281c2bbebe6860f672
ultracoolclub <jeremiah.ashcraft@gmail.com>
2020-03-24 00:20:49 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,41 @@
+const moonPath = 
+  "M15.5 27.5C15.5 42.6878 27.5 55 27.5 55C12.3122 55 0 42.6878 0 27.5C0 12.3122 12.3122 0 27.5 0C27.5 0 15.5 12.3122 15.5 27.5Z";
+
+
+const sunPath = "M55 27.5C55 42.6878 42.6878 55 27.5 55C12.3122 55 0 42.6878 0 27.5C0 12.3122 12.3122 0 27.5 0C42.6878 0 55 12.3122 55 27.5Z";
+
+const darkMode = document.querySelector("#darkMode");
+let toggle = false;
+
+//click on the sun//
+
+darkMode.addEventListener('click', () => {
+  //anime.js//
+  //Set up the timeline//
+  const timeline = anime.timeline({
+    duration : 750,
+    easing : "easeOutExpo"
+  });
+  //Add different animations to the timeline//
+  timeline.add({
+    targets: ".sun",
+    d: [
+      {value: toggle ? sunPath : moonPath}
+    ]
+  })
+  .add({
+    targets: "#darkMode",
+    rotate: 320
+  }, '-= 350')
+  .add({
+    targets: "section",
+    backgroundColor: toggle ? 'rgb(199, 199, 199)' :'rgb(22, 22, 22)',
+    color: toggle ? "rgb(22,22,22)" : "rgb(199, 199, 199)"
+  }, '-= 700')
+  //Everytime we click on the sun the toggle will switch//
+  if(!toggle){
+    toggle = true;
+  }else{
+    toggle = false;
+  }
+});
